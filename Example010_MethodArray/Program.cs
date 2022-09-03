7c5f0e913fbffb8e1254ee15c11d05a533b8dcd9
Polina Deniskina <polinaoioi67@gmail.com>
2022-09-03 19:49:31 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 45, 24, 4, 6, 59, 6, 8, 17 };
+
+int n = array.Length;
+int find = 6;
+
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
+
